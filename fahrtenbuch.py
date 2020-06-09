18c440b106b270517577c18b6923e3317bd09d0f
benedikt-wehner <bewe777@gmail.com>
2020-06-10 00:06:11 +0200

    Datepicker hinzu. Funktion noch ausstehend

--- fahrtenbuch.py
+++ fahrtenbuch.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import QDate
 import sqlite3
 
 
@@ -7,14 +8,14 @@ class Window(QtGui.QMainWindow):
     
     def __init__(self):
         super().__init__()
-        self.setGeometry(50, 50, 300, 300) #Fenster initialisieren, Größe festlegen
+        self.setGeometry(50, 50, 550, 300) #Fenster initialisieren, Größe festlegen
         self.setWindowTitle("Fahrtenbuch") #Titel für das Fenster festlegen
         self.elements()
 
     def elements(self):
         #Dieses Objekt enhält alle Steuerelemente der Software.
         '''Es sind enthalten:
-        - Kilometerstand
+        - Kilometerstand*
         - Datum *
         - Preis *
         - Preis pro Liter *
@@ -28,14 +29,17 @@ class Window(QtGui.QMainWindow):
         self.lineInKilometerstand.move(50, 10)
         self.lineInKilometerstand.resize(200, 30)
         self.lineInKilometerstand.setPlaceholderText("Kilometerstand")
-        self.lineInKilometerstand.setValidator(QtGui.QIntValidator(0, 100000, self))
+        self.lineInKilometerstand.setValidator(QtGui.QIntValidator(0, 1000000, self))
         self.lineInKilometerstand.editingFinished.connect(self.textAngepasst)
 
         #Datum
-        lineInDatum = QtGui.QLineEdit(self)
-        lineInDatum.move(50, 10+vertikalerAbstand)
-        lineInDatum.resize(200, 30)
-        lineInDatum.setPlaceholderText("Datum")
+        self.lineInDatum = QtGui.QCalendarWidget(self)
+        self.lineInDatum.setFirstDayOfWeek(self, QtDayOfWeek=1)
+        self.lineInDatum.setGridVisible(True)
+        #self.lineInDatum = QtGui.QLineEdit(self)
+        self.lineInDatum.move(300, 10)
+        self.lineInDatum.resize(200, 30)
+        #self.lineInDatum.setPlaceholderText("Datum")
         '''
         !!!!Das Datum kann aktuell auch falsch eingelesen werden! Es findet keine Filterung statt!!!
         '''
@@ -44,37 +48,49 @@ class Window(QtGui.QMainWindow):
 
 
         #Preis
-        lineInPreis = QtGui.QLineEdit(self)
-        lineInPreis.move(50, 10+(2*vertikalerAbstand))
-        lineInPreis.resize(200, 30)
-        lineInPreis.setPlaceholderText("Betrag gezahlt")
+        self.lineInPreis = QtGui.QLineEdit(self)
+        self.lineInPreis.move(50, 10+(1*vertikalerAbstand))
+        self.lineInPreis.resize(200, 30)
+        self.lineInPreis.setPlaceholderText("Betrag gezahlt")
 
 
         #Preis pro Liter
-        lineInGrundpreis = QtGui.QLineEdit(self)
-        lineInGrundpreis.move(50, 10+(3*vertikalerAbstand))
-        lineInGrundpreis.resize(200, 30)
-        lineInGrundpreis.setPlaceholderText("Grundpreis in €/l")
+        self.lineInGrundpreis = QtGui.QLineEdit(self)
+        self.lineInGrundpreis.move(50, 10+(2*vertikalerAbstand))
+        self.lineInGrundpreis.resize(200, 30)
+        self.lineInGrundpreis.setPlaceholderText("Grundpreis in €/l")
 
-        btnSchreibeDB = QtGui.QPushButton("Schreibe in DB", self)
-        btnSchreibeDB.clicked.connect(self.schreibeDatenbank)
-        btnSchreibeDB.move(50, 10+(4*vertikalerAbstand))
-        btnSchreibeDB.resize(150, 30)
+        self.btnSchreibeDB = QtGui.QPushButton("Schreibe in DB", self)
+        self.btnSchreibeDB.clicked.connect(self.schreibeDatenbank)
+        self.btnSchreibeDB.move(50, 10+(4*vertikalerAbstand))
+        self.btnSchreibeDB.resize(150, 30)
+
+        #Button zum Lesen der DB Daten
+        self.btnLeseDB = QtGui.QPushButton("Lese DB", self)
+        self.btnLeseDB.clicked.connect(self.schreibeDatenbank)
+        self.btnLeseDB.move(50, 10+(5*vertikalerAbstand))
+        self.btnLeseDB.resize(150, 30)
 
 
 
         self.show()
 
 
-    feldLeer = True
+    
     def textAngepasst(self):
         feldLeer = False
         print(self.lineInKilometerstand.text())
 
-    kilometerstandAlt = 0
+    def leseDatenbank(self):
+        self.databaseConnection()
+
+
     def schreibeDatenbank(self):
 
-        
+        '''
+
+        global kilometerstandAlt
+        global feldLeer
         #kilometerstandAlt = self.lineInKilometerstand.text()
         if (kilometerstandAlt == self.lineInKilometerstand.text()) or feldLeer == True:
             print("Fehler. Neue Daten eingeben")
@@ -83,11 +99,73 @@ class Window(QtGui.QMainWindow):
             print("test")
             kilometerstandAlt = self.lineInKilometerstand.text()
         
+        '''
+
+        self.databaseConnection()
+        self.databaseCreateTable()
+
+        kilometerstand = self.lineInKilometerstand.text()
+        datum = self.lineInDatum.text()
+        preis = self.lineInPreis.text()
+        literpreis = self.lineInGrundpreis.text()
+        typ = "test"
+
+        if kilometerstand == "" or datum == "" or typ == "":
+            print("Fehler")
+        else:
+            self.databaseConnection()
+            #sql_schreibe_daten=
+            self.zeiger.execute("""
+                INSERT INTO tanken
+                    VALUES (?,?,?,?,?)
+                    """,
+                    (kilometerstand, datum, preis, literpreis, typ))
+
+            self.verbindung.commit()
+            self.verbindung.close()
+
+
+
+    def databaseConnection(self):
+
+        self.verbindung = sqlite3.connect("/home/benedikt/Schreibtisch/digitalesFahrtenbuch/DB/data.db")
+        self.zeiger = self.verbindung.cursor()
+
+    def databaseCreateTable(self):
+
+        sql_tabelle_erstellen = """
+        CREATE TABLE IF NOT EXISTS tanken (
+            kilometerstand INTEGER,
+            datum DATE,
+            preis REAL,
+            literpreis REAL,
+            typ VARCHAR(20)
+        );
+        """
+
+        self.zeiger.execute(sql_tabelle_erstellen)
+        self.verbindung.commit()
+
+        self.zeiger.execute("SELECT * FROM tanken")
+        inhalt = self.zeiger.fetchall()
+        print(inhalt)
+        
+        
+        self.verbindung.close()
+        
+
+
+        
 
 
 def run():
+    
     app = QtGui.QApplication(sys.argv)
     gui = Window()
     sys.exit(app.exec_())
 
+
+
+feldLeer = True
+kilometerstandAlt = 0
 run()
